d7bd3fbc17f9241d3babae8a778dbc4f1ba03071
Carlos Galan <cgalan23@ilg.cat>
2024-05-16 16:26:04 +0200

    ParameterizedTest

--- src/test/java/N1EX2_Test/N1EX2_Test.java
+++ src/test/java/N1EX2_Test/N1EX2_Test.java
@@ -1,55 +1,36 @@
 package N1EX2_Test;
 
-import java.util.ArrayList;
 import java.util.stream.Stream;
-
 import N1EX2.N1EX2;
 import org.junit.Assert;
-import org.junit.Test;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.ValueSource;
-
-import static N1EX2.N1EX2.calculoDni;
-import static N1EX2.N1EX2.dniList;
 
 public class N1EX2_Test {
     private N1EX2 list = new N1EX2();
 
-    @BeforeEach
-    void createListForTests() {
-        //ArrayList<Integer> numList= list.dniList();
-    }
-
-    @Test
-    public void testCalculo(){
-        ArrayList<Integer> dniList = dniList();
-        String letterDni = "";
-
-        // Expected result
-        String[] vectorLetter = {"Z", "X", "L", "M", "X", "G", "M", "H", "B", "N"};
-
-        for (int i = 0; i < dniList.size(); i++) {
-            letterDni = calculoDni(dniList.get(i));
-            Assert.assertEquals(vectorLetter[i],letterDni);
-        }
-    }
     @ParameterizedTest
-    @ValueSource(ints = {12345678,87654321, 12348765, 87651234, 12783456, 87126534,
-            98765432, 12093487, 90785634, 21436587})
-    public void testCalculo2(int dni){
+    @MethodSource("provideParameters")
+    public void testCalculo(int dni, String letter){
         // Expected result
         String letterDni = list.calculoDni(dni);
-        String[] vectorLetter = letras();
 
-        Assert.assertEquals(vectorLetter,letterDni);
+        Assert.assertEquals(letter,letterDni);
     }
 
-    public String[] letras(){
-        String[] vectorLetter = {"Z", "X", "L", "M", "X", "G", "M", "H", "B", "N"};
-
-        return vectorLetter;
+    private static Stream<Arguments> provideParameters() {
+        return Stream.of(
+                Arguments.of(12345678,"Z"),
+                Arguments.of(87654321,"X"),
+                Arguments.of(12348765,"L"),
+                Arguments.of(87651234,"M"),
+                Arguments.of(12783456,"X"),
+                Arguments.of(87126534,"G"),
+                Arguments.of(98765432,"M"),
+                Arguments.of(12093487,"H"),
+                Arguments.of(90785634,"B"),
+                Arguments.of(21436587,"N")
+        );
     }
 }
