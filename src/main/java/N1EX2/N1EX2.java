c9ac26048785ac35f1c927d509fa4e285b0291b9
Carlos Galan <cgalan23@ilg.cat>
2024-05-16 16:45:30 +0200

    Last modification of the N1EX2_Test

--- src/main/java/N1EX2/N1EX2.java
+++ src/main/java/N1EX2/N1EX2.java
@@ -3,10 +3,6 @@ package N1EX2;
 import java.util.ArrayList;
 
 public class N1EX2 {
-    /*Crea una clase llamada CalculoDni que calcule la letra del DNI al recibir el número como parámetro.
-    Crea una clase jUnit que verifique su correcto funcionamiento, parametrizándola para que el test reciba
-    un espectro de datos amplio y valide si el cálculo es correcto para 10 números de DNI predefinidos*/
-
     public static void runProgram(){
         int dni1 = 12345678;
         String letterDni = "";
